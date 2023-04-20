cd9b75832999c2891e0d67fa77e7e4c096ddd060
Mohit Bhadauria <thakurmohitbhadauria@gmail.com>
2023-04-20 15:33:17 +0530

    Update 1. Write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included)..py

--- 1. Write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included)..py
+++ 1. Write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included)..py
@@ -1,4 +1,4 @@
-Numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included). 
+1. Numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included). 
 l = []
 for i in range(1500,2701):
     if (i%7==0) and (i%5==0):
